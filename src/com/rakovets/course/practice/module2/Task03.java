d6c79056c295f34784d2f87382c939a88154f104
giveXp <55504691+giveXp@users.noreply.github.com>
2019-09-18 21:35:05 +0300

    lesson3try1

--- src/com/rakovets/course/practice/module2/Task03.java
+++ src/com/rakovets/course/practice/module2/Task03.java
@@ -11,8 +11,8 @@ public class Task03 {
 	public static void main(String[] args) {
 		//FIXME
 		// Переменные, которые можно изменять для проверки различных вариантов входных параметров
-		int penaltyPointFirstPlayer = 1;
-		int penaltyPointSecondPlayer = 2;
+		int penaltyPointFirstPlayer = 25;
+		int penaltyPointSecondPlayer = 43;
 
 		// Код необходимый для тестирования, не изменять
 		penaltyPointFirstPlayer = (args.length == 0) ? penaltyPointFirstPlayer : Integer.valueOf(args[0]);
@@ -21,5 +21,9 @@ public class Task03 {
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+		System.out.println(Math.min(penaltyPointFirstPlayer , penaltyPointSecondPlayer));
+
+
+
 	}
 }
